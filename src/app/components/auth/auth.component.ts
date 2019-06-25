d5f8e1dc11590de5a0a3acede445876d3930a3b9
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2019-06-25 16:09:37 -0500

    Creating auth service.

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,76 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from '../../../environments/environment';
 import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { AppStateInterface } from 'src/app/store/states/app.state';
-import { AuthStateInterface, InitialAuthState } from './auth.model';
-import { AuthSetCredentialsAction } from 'src/app/store/actions/auth.actions';
+import { SpotifyAuthService } from 'src/app/services/spotify-auth.service';
+import { selectAuthState } from 'src/app/store/selectors/index';
 @Component({
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss']
 })
 export class AuthComponent implements OnInit {
-  token = '';
-  authData: AuthStateInterface = InitialAuthState;
-  queryFragment = [];
-  spotify = {
-    scopes: [
-      'streaming',
-      'user-read-birthdate',
-      'user-read-email',
-      'user-read-private'
-    ],
-    SAuthorizeUrl: 'https://accounts.spotify.com/authorize',
-    appAuthUrl: 'https://localhost:3000/auth'
-  };
-
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private store: Store<AppStateInterface>
+    private store: Store<AppStateInterface>,
+    private spotifyAuthService: SpotifyAuthService
   ) {}
 
   ngOnInit() {
     this.getAuthDataFromQString();
-    if (this.authData.access_token) {
-      this.store.dispatch(new AuthSetCredentialsAction(this.authData));
-      this.router.navigate(['/']);
-    }
+    this.store.select(selectAuthState).subscribe(authData => {
+      if (authData.access_token) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
   getAuthDataFromQString() {
-    this.queryFragment =
-      this.route.snapshot.fragment && this.route.snapshot.fragment.split('&');
-
-    if (!this.queryFragment) {
+    if (!this.route.snapshot.fragment) {
       return;
     }
-
-    this.queryFragment.forEach(str => {
-      const [key, value] = str.split('=');
-      this.authData[key] = value;
-    });
-    console.log(this.authData);
+    this.spotifyAuthService.saveAuthDataFromQueryFragment(
+      this.route.snapshot.fragment
+    );
   }
 
   getSpotifyAutorization() {
-    const qp = {
-      redirect_uri: encodeURIComponent(this.spotify.appAuthUrl),
-      scope: encodeURIComponent(this.spotify.scopes.join(' ')),
-      response_type: 'token',
-      state: 'dz'
-    };
-
-    let queryString = `?client_id=${encodeURIComponent(
-      environment.spotify.auth.clientId
-    )}`;
-
-    Object.keys(qp).forEach(param => {
-      queryString = queryString + `&${param}=${qp[param]}`;
-    });
-
-    window.location.href = `${this.spotify.SAuthorizeUrl}${queryString}`;
+    this.spotifyAuthService.requestSpotifyAccess();
   }
 }
