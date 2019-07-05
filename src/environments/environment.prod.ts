4e14654741ace890e3578f8b41984800d1a4dc89
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2019-07-05 12:23:10 -0500

    adding production settings.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,16 @@
 export const environment = {
-  production: true
+  production: true,
+  spotify: {
+    auth: {
+      clientId: '09b24f2eda9e45d3a815f95d88a129e1',
+      authorizeUrl: 'https://accounts.spotify.com/authorize',
+      callbackUrl: 'https://visionplayer.danielzamorano.pro/auth'
+    }
+  },
+  unsplash: {
+    baseUrl: 'https://unsp-api-stg.herokuapp.com',
+    endpoints: {
+      collection: '/collection'
+    }
+  }
 };
