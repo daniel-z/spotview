3e53551fdfbb153eedc896f8d7d31c1ada9f227d
Daniel Zamorano <daniel.zamorano.m@gmail.com>
2019-07-08 16:26:27 -0500

    Implementing previous and next player actions

--- src/app/store/actions/player.actions.ts
+++ src/app/store/actions/player.actions.ts
@@ -2,12 +2,25 @@ import { Action } from '@ngrx/store';
 import { PlayerStateInterface } from '../../components/player/player.model';
 
 export enum PlayerActions {
-  PLAYER_STATE_CHANGE = 'PLAYER_STATE_CHANGE'
+  PLAYER_STATE_CHANGE = 'PLAYER_STATE_CHANGE',
+  PLAYER_NEXT = 'PLAYER_NEXT',
+  PLAYER_PREVIOUS = 'PLAYER_PREVIOUS'
 }
 
 export class PlayerStateChangeAction implements Action {
   readonly type: string = PlayerActions.PLAYER_STATE_CHANGE;
-  constructor(public payload: PlayerStateInterface) {}
+  constructor() {}
 }
 
-export type PlayerActionType = PlayerStateChangeAction;
+export class PlayerNextAction implements Action {
+  readonly type: string = PlayerActions.PLAYER_NEXT;
+}
+
+export class PlayerPreviousAction implements Action {
+  readonly type: string = PlayerActions.PLAYER_PREVIOUS;
+}
+
+export type PlayerActionType =
+  | PlayerStateChangeAction
+  | PlayerPreviousAction
+  | PlayerNextAction;
